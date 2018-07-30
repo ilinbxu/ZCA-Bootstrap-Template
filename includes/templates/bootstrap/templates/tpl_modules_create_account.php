b55f329cbc4d0e6938adb1a98a205c8cd082420c
ilinbxu <ilinbxu@126.com>
2018-07-30 10:56:12 +0800

    Customer Details  State - Always display as pulldown?  true
    If the customer registration information is turned on [State - Always display as pulldown?], but the corresponding country has no region, an error will be reported.

--- includes/templates/bootstrap/templates/tpl_modules_create_account.php
+++ includes/templates/bootstrap/templates/tpl_modules_create_account.php
@@ -105,19 +105,20 @@
 <?php echo zen_draw_input_field('city', '', zen_set_field_length(TABLE_ADDRESS_BOOK, 'entry_city', '40') . ' id="city" placeholder="' . ENTRY_CITY_TEXT . '"'. ((int)ENTRY_CITY_MIN_LENGTH > 0 ? ' required' : '')); ?>
 <div class="p-2"></div>
 
-<?php
+<?php 
   if (ACCOUNT_STATE == 'true') {
     if ($flag_show_pulldown_states == true) {
 ?>
-<label class="inputLabel" for="stateZone" id="zoneLabel"><?php echo ENTRY_STATE; ?></label><?php if (zen_not_null(ENTRY_STATE_TEXT)) echo '<span class="alert">' . ENTRY_STATE_TEXT . '</span>';?>
-
+<label class="inputLabel" for="stateZone" id="zoneLabel"><?php echo ENTRY_STATE; ?></label><?php if (zen_not_null(ENTRY_STATE_TEXT)) echo '&nbsp;<span class="alert">' . ENTRY_STATE_TEXT . '</span>';?>
 <?php
       echo zen_draw_pull_down_menu('zone_id', zen_prepare_country_zones_pull_down($selected_country), $zone_id, 'id="stateZone"');
+      
+    }
 ?>
 
-
-<?php  } else { ?>
-
+<?php if ($flag_show_pulldown_states == true) { ?>
+<div class="p-2"></div>
+<?php } ?>
 <label class="inputLabel" for="state" id="stateLabel"><?php echo $state_field_label; ?></label>
 <?php
     echo zen_draw_input_field('state', '', zen_set_field_length(TABLE_ADDRESS_BOOK, 'entry_state', '40') . ' id="state" placeholder="' . ENTRY_STATE_TEXT . '"');
@@ -125,12 +126,10 @@
       echo zen_draw_hidden_field('zone_id', $zone_name, ' ');
     }
 ?>
-
+<div class="p-2"></div>
 <?php
   }
-}
 ?>
-<div class="p-2"></div>
 
 <label class="inputLabel" for="postcode"><?php echo ENTRY_POST_CODE; ?></label>
 <?php echo zen_draw_input_field('postcode', '', zen_set_field_length(TABLE_ADDRESS_BOOK, 'entry_postcode', '40') . ' id="postcode" placeholder="' . ENTRY_POST_CODE_TEXT . '"' . ((int)ENTRY_POSTCODE_MIN_LENGTH > 0 ? ' required' : '')
